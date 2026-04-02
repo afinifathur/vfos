f82edb6dd13d8ab4ec79b4ac74323dea2939b796
Afini Fathur <148167126+afinifathur@users.noreply.github.com>
2026-04-03 06:59:29 +0700

    perbaikan API saham www.bareksa.com

--- app/Services/BareksaService.php
+++ app/Services/BareksaService.php
@@ -20,7 +20,7 @@ class BareksaService
                 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'Accept-Language' => 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
-                'Accept-Encoding' => 'gzip, deflate, br',
+                'Accept-Encoding' => 'gzip, deflate',
                 'Cache-Control' => 'no-cache',
                 'Pragma' => 'no-cache',
                 'Sec-Ch-Ua' => '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
